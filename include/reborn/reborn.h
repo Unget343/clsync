4031e86d4f627ef68b30490c9d21e3131c15b802
unget343 <zaharkovtonuk7@gmail.com>
2026-08-19 15:28:52 +0300

    feat: introduce cross-platform socket abstraction layer in reborn.h

--- include/reborn/reborn.h
+++ include/reborn/reborn.h
@@ -3,17 +3,30 @@
 
 #ifdef __TEST__
 #   define __REB_TEST__ 1
-#   include <cstring>
-#   include <string>
+#endif
+
+#include <cstring>
+#include <string>
+#include <vector>
+
+#ifdef _WIN32
+#   include <winsock2.h>
+#   include <ws2tcpip.h>
+#   include <afunix.h>
+#   include <windows.h>
+#   pragma comment(lib, "ws2_32.lib")
+    typedef SOCKET SOCKET_FD;
+#   define INVALID_SOCKET_VALUE INVALID_SOCKET
+#else 
 #   include <unistd.h>
 #   include <sys/socket.h>
 #   include <sys/stat.h>
 #   include <sys/un.h>
-#   include <vector>
-using namespace std;
-#else 
-// include windows api
+    typedef int SOCKET_FD;
+#   define INVALID_SOCKET_VALUE (-1)
 #endif
+
+using namespace std;
 
 enum SocketStat
 {
@@ -49,8 +62,8 @@ namespace reborn
             static constexpr size_t kMaxBufferRequests = 10;
 
         protected:
-            int server_fd = -1;
-            int client_fd = -1;
+            SOCKET_FD server_fd = INVALID_SOCKET_VALUE;
+            SOCKET_FD client_fd = INVALID_SOCKET_VALUE;
             ssize_t bytes = 0;
             SocketStat last_status = UNDEFINED;
             const char* answer = nullptr;
@@ -64,8 +77,14 @@ namespace reborn
             Socket()
             {
                 buffer.reserve(kMaxBufferRequests);
+#ifdef _WIN32
+                WSADATA wsaData;
+                if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+                    perror("[-500]: Reborn: WSAStartup failed");
+                }
+#endif
                 server_fd = ::socket(AF_UNIX, SOCK_STREAM, 0);
-                if (server_fd == -1)
+                if (server_fd == INVALID_SOCKET_VALUE)
                     perror("[-500]: Reborn: cannot starting IPC Socket");
             }
 
@@ -73,8 +92,14 @@ namespace reborn
             Socket(const char* _path) : socket_path(_path)
             {
                 buffer.reserve(kMaxBufferRequests);
+#ifdef _WIN32
+                WSADATA wsaData;
+                if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+                    perror("[-500] Reborn: WSAStartup failed");
+                }
+#endif
                 server_fd = ::socket(AF_UNIX, SOCK_STREAM, 0);
-                if (server_fd == -1)
+                if (server_fd == INVALID_SOCKET_VALUE)
                     perror("[-500] Reborn: cannot starting IPC Socket");
             }
 
@@ -91,20 +116,39 @@ namespace reborn
             { return answer; }
 
             explicit operator bool() const 
-            { return server_fd != -1; }
+            { return server_fd != INVALID_SOCKET_VALUE; }
 
             bool operator!() const 
-            { return server_fd == -1; }
+            { return server_fd == INVALID_SOCKET_VALUE; }
 
             ~Socket()
             {
-                if (server_fd != -1)
+                if (server_fd != INVALID_SOCKET_VALUE) {
+#ifdef _WIN32
+                    ::closesocket(server_fd);
+#else
                     ::close(server_fd);
-                if (client_fd != -1)
+#endif
+                }
+                if (client_fd != INVALID_SOCKET_VALUE) {
+#ifdef _WIN32
+                    ::closesocket(client_fd);
+#else
                     ::close(client_fd);
+#endif
+                }
 
-                if (socket_path)
-                    unlink(socket_path);
+                if (socket_path) {
+#ifdef _WIN32
+                    ::_unlink(socket_path);
+#else
+                    ::unlink(socket_path);
+#endif
+                }
+
+#ifdef _WIN32
+                WSACleanup();
+#endif
             }
         };
 } // reborn
